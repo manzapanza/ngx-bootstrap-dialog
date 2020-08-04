f03c15381e227a61d8a5b1bb98305a2a89f5028b
Massimo Rangoni <maxappbr@gmail.com>
2020-08-04 03:40:21 -0300

    feat(demo): Add two base examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { NgxBootstrapDialogService } from './../../projects/ngx-bootstrap-dialog/src/lib/ngx-bootstrap-dialog.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngx-bootstrap-dialog-demo';
+  constructor(private ngxBootstrapDialogService: NgxBootstrapDialogService) {}
+
+  openDialogAlert() {
+    this.ngxBootstrapDialogService
+      .alert({
+        title: 'Alert title',
+        message: 'Message alert',
+      })
+      .then(() => {
+        console.log('OK');
+      })
+      .catch(() => {
+        console.log('Cancel');
+      });
+  }
+
+  openDialogConfirm() {
+    this.ngxBootstrapDialogService
+      .confirm({
+        title: 'Title',
+        message: 'Message',
+      })
+      .then(() => {
+        console.log('OK');
+      })
+      .catch(() => {
+        console.log('Cancel');
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxBootstrapDialogModule } from './../../projects/ngx-bootstrap-dialog/src/lib/ngx-bootstrap-dialog.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxBootstrapDialogModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
