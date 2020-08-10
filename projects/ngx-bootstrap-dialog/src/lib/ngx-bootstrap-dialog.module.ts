a9bad00f547c191311f22a5f219cc66c9169fc68
Massimo Rangoni <maxappbr@gmail.com>
2020-08-09 23:26:33 -0300

    fix: export types module

--- projects/ngx-bootstrap-dialog/src/lib/ngx-bootstrap-dialog.module.ts
+++ projects/ngx-bootstrap-dialog/src/lib/ngx-bootstrap-dialog.module.ts
@@ -5,6 +5,14 @@ import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxBootstrapDialogService } from './ngx-bootstrap-dialog.service';
 import { NgxBootstrapDialogComponent } from './ngx-bootstrap-dialog/ngx-bootstrap-dialog.component';
 
+export {
+  NgxBootstrapDialogOptions,
+  NgxBootstrapDialogAlertDeafultOptions,
+  NgxBootstrapDialogConfirmDefaultOptions,
+  NGX_BOOTSTRAP_ALERT_DEFAULT_OPTIONS,
+  NGX_BOOTSTRAP_CONFIRM_DEFAULT_OPTIONS,
+} from './ngx-bootstrap-dialog.config';
+
 @NgModule({
   imports: [CommonModule, NgbModalModule],
   declarations: [NgxBootstrapDialogComponent],
